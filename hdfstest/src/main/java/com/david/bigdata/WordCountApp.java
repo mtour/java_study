388d11cb38d64746b3a0b41e48c08ba3f4adb49b
cwliu1986 <cwliu1986@gmail.com>
2020-02-14 20:55:59 +0800

    判读输出目录是否存在

--- hdfstest/src/main/java/com/david/bigdata/WordCountApp.java
+++ hdfstest/src/main/java/com/david/bigdata/WordCountApp.java
@@ -1,6 +1,7 @@
 package com.david.bigdata;
 
 import org.apache.hadoop.conf.Configuration;
+import org.apache.hadoop.fs.FileSystem;
 import org.apache.hadoop.fs.Path;
 import org.apache.hadoop.io.IntWritable;
 import org.apache.hadoop.io.Text;
@@ -10,6 +11,7 @@ import org.apache.hadoop.mapreduce.lib.output.FileOutputFormat;
 
 import javax.security.auth.login.AppConfigurationEntry;
 import java.io.IOException;
+import java.net.URI;
 
 public class WordCountApp
 {
@@ -28,8 +30,14 @@ public class WordCountApp
         job.setOutputKeyClass(Text.class);
         job.setOutputValueClass(IntWritable.class);
 
+        FileSystem fs = FileSystem.get(new URI("hdfs://192.168.56.101:9000"),configuration);
+        Path outPutPath = new Path("/wordcount/output");
+        if (fs.exists(outPutPath)){
+            fs.delete(outPutPath,true);
+        }
+
         FileInputFormat.setInputPaths(job,new Path("/wordcount/input"));
-        FileOutputFormat.setOutputPath(job,new Path("/wordcount/output"));
+        FileOutputFormat.setOutputPath(job,outPutPath);
 
         boolean result = job.waitForCompletion(true);
         System.exit(result?0 : -1);
